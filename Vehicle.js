cf4bad4e708f2bbf572096c928cdb98d5e4c249f
Ubuntu <ubuntu@ip-172-31-33-14.ec2.internal>
2021-01-04 15:19:49 +0000

    added program

---
+++ Vehicle.js
@@ -0,0 +1,49 @@
+/*
+ * The vehicle program implements an application that
+ * creates an object.
+ *
+ * @author  Ben Whitten
+ * @version 1.0
+ * @since   2021-1-4
+ */
+
+///////////////////////////////////////////////////////////////////////////////
+
+// Defining require and process
+/*eslint no-undef: "error"*/
+/*global module*/
+// Defining prompt for getting user input.
+
+class Vehicle {
+
+  constructor() {
+    this.speed = 40;
+    this.licenseplateNumber;
+    this.colour;
+    this.numberOfDoors = 4;
+    this.maxSpeed = 100;
+    this.speedIncreaseDecrease = 10;
+  }
+
+  getSpeed() {
+    return this.speed;
+  }
+
+  accelerate() {
+    this.speed += this.speedIncreaseDecrease;
+    if (this.speed > this.maxSpeed) {
+      this.speed = this.maxSpeed;
+    }
+  }
+
+  brake() {
+    this.speed -= this.speedIncreaseDecrease;
+    if (this.speed < 0) {
+      this.speed = 0;
+    }
+  }
+}
+// Exporting the class
+module.exports = Vehicle;
+
+///////////////////////////////////////////////////////////////////////////////
